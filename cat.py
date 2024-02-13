ec09a631f15f2a6418b263bc72a2badc9213be22
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-13 12:45:57 -0600

    Update cat.py

--- cat.py
+++ cat.py
@@ -3,9 +3,19 @@ class Cat():
     def _init_(self):
         self.name="Unknown"
         self.age= 0 
+
     def speak(self):
         print("Meow")
+
+
 
 #Create objects here
 #These should NOT be indented inside the class
 
+stella = Cat()
+stella.name = "Stella"
+stella.age = 7
+
+garfield= Cat()
+garfield.name = "Garfield"
+garfield.age = 50
