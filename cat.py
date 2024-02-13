db76e6e773d96b207391d67c2dd51611fc11d9f7
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-13 12:23:28 -0600

    Update cat.py
    fixed a def

--- cat.py
+++ cat.py
@@ -3,9 +3,8 @@ class Cat():
     def _init_(self):
         self.name="unknown"
         self.age= 0 
-
-def speak(self):
-    print("Meow")
+    def speak(self):
+        print("Meow")
 
 
 #Create objects here
